55dd028e4631effc8b83c49da6f0ab31a57f8bbc
S-JOBI23-058\student <231064@st.yoshida-g.ac.jp>
2024-03-25 16:31:57 +0900

    タイトル画面の作成、ゲーム開始カウントダウン処理

--- Assets/02_Scripts/05_UI/UiManager.cs
+++ Assets/02_Scripts/05_UI/UiManager.cs
@@ -10,20 +10,28 @@ using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
+    [SerializeField] GameObject timerTextObj;       //タイマーテキストのオブジェクト
     [SerializeField] Text timerText;                //タイマーのテキスト
     [SerializeField] float timerSeconds;            //タイマー（秒）
     [SerializeField] int timerMinutes;              //タイマー（分）
+    [SerializeField] bool isEnd;                    //カウントダウンが終わったかどうか
     [SerializeField] GameObject gameEndText;        //ゲーム終了テキスト
+    [SerializeField] Text startTimerText;           //ゲーム開始カウントダウンテキスト
+    [SerializeField] float startTimer;                //ゲーム開始カウントダウン
 
     // Start is called before the first frame update
     void Start()
     {
         //ゲーム終了テキストを非表示
         gameEndText.SetActive(false);
+        //タイマーを非表示
+        timerTextObj.SetActive(false);
         //タイマー（分）の設定
         timerMinutes = 2;
         //タイマー（秒）を　分*60　で設定
         timerSeconds = timerMinutes * 60;
+        //１秒ごとに関数を呼ぶ
+        InvokeRepeating("CountDown", 1.0f, 1.0f);
     }
 
     // Update is called once per frame
@@ -34,19 +42,37 @@ public class UiManager : MonoBehaviour
         //    Initiate.Fade("SampleScene", Color.black, 1.0f);
         //}
 
-        //カウントダウン
-        timerSeconds -= Time.deltaTime;
-        //インスタンスを生成
-        var span = new TimeSpan(0, 0, (int)timerSeconds);
-        //フォーマットする
-        timerText.text = span.ToString(@"m\:ss");
-
-        if(timerSeconds < 0)
+        if (isEnd)
         {
-            timerSeconds = 0;
+            timerTextObj.SetActive(true);
+            //カウントダウン
+            timerSeconds -= Time.deltaTime;
+            //インスタンスを生成
+            var span = new TimeSpan(0, 0, (int)timerSeconds);
+            //フォーマットする
+            timerText.text = span.ToString(@"m\:ss");
 
-            //ゲーム終了テキストを表示
-            gameEndText.SetActive(true);
+            if (timerSeconds < 0)
+            {
+                timerSeconds = 0;
+
+                //ゲーム終了テキストを表示
+                gameEndText.SetActive(true);
+            }
+        }
+    }
+
+    //カウントダウン処理
+    void CountDown()
+    {
+        --startTimer;
+        startTimerText.text = startTimer.ToString();
+        if (startTimer < 0)
+        {
+            startTimer = 0;
+            isEnd = true;
+            Destroy(startTimerText);
+            CancelInvoke();
         }
     }
 }
